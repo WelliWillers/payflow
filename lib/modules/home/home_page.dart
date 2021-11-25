2dde09b8e93e92d8ad07a83394f869dc38fd0bfd
Wellington Willers <wellington.willers@gmail.com>
2021-11-25 20:35:44 -0300

    home bottonNavigation pronto

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,9 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_controller.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  final homecontroller = HomeController();
+  final pages = [
+    Container (
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    )
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -15,16 +32,19 @@ class HomePage extends StatelessWidget {
           color: AppColors.primary,
           child: Center(
             child: ListTile(
-              title: Text.rich(
-                TextSpan(
-                  text:"Olá, ", style: TextStyles.titleRegular,
+              title: Text.rich(TextSpan(
+                  text: "Olá, ",
+                  style: TextStyles.titleRegular,
                   children: [
                     TextSpan(
-                      text:"Gabu", style: TextStyles.titleBoldBackground,
+                      text: "Gabu",
+                      style: TextStyles.titleBoldBackground,
                     )
-                  ])
+                  ])),
+              subtitle: Text(
+                "Mantenha suas contas em dia",
+                style: TextStyles.captionShape,
               ),
-              subtitle: Text("Mantenha suas contas em dia", style: TextStyles.captionShape,),
               trailing: Container(
                 height: 48,
                 width: 48,
@@ -37,6 +57,52 @@ class HomePage extends StatelessWidget {
           ),
         ),
       ),
+      bottomNavigationBar: Container(
+        height: 90,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+                onPressed: () {
+                  homecontroller.setPage(0);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.home,
+                  color: AppColors.primary,
+                )),
+            GestureDetector(
+              onTap: (){
+                print("Clicou");
+              },
+              child: Container(
+                width: 56,
+                height: 56,
+                decoration: BoxDecoration(
+                  color: AppColors.primary,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: IconButton(
+                    onPressed: () {},
+                    icon: Icon(
+                      Icons.add_box_outlined,
+                      color: AppColors.background,
+                    )),
+              ),
+            ),
+            IconButton(
+                onPressed: () {
+                  homecontroller.setPage(1);
+                  setState(() {});
+                },
+                icon: Icon(
+                  Icons.description_outlined,
+                  color: AppColors.body,
+                )),
+          ],
+        ),
+      ),
+      body: pages[homecontroller.currentPage],
     );
   }
 }
