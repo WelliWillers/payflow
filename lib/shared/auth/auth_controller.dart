6f9772623122e28439057fc536ecacd4c6d58416
Wellington Willers <wellington.willers@gmail.com>
2021-11-25 19:12:20 -0300

    autintecação google

---
+++ lib/shared/auth/auth_controller.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/modules/home/home_page.dart';
+import 'package:payflow/modules/login/login_page.dart';
+
+class AuthController {
+  var _isAutenticated = false;
+  var _user;
+
+  get user => _user;
+
+  void setUser(BuildContext context, var user){
+    if(user != null){
+      _user = user;
+      _isAutenticated = true;
+      Navigator.pushReplacementNamed(context, "/home");
+    } else {
+      _isAutenticated = false;
+      Navigator.pushReplacementNamed(context, "/login");
+    }
+  }
+}
